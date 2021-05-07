557c8392025ba66bbe56503f0816b1c37f414915
abhishek <abhishek@mobiux.in>
2021-05-08 01:07:16 +0530

    github

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,26 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/abhisheksy/abhisheksy.github.io/gh-pages/' : '',
-}
+module.exports = 
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/abhisheksy.github.io/_next/static/images/',
+  }],
+  {
+	  // Use the CDN in production and localhost for development.
+	  // assetPrefix: isProd ? 'https://cdn.statically.io/gh/abhisheksy/abhisheksy.github.io/gh-pages/' : '',
+	  assetPrefix: '/abhisheksy.github.io/',
+	  basePath: '/abhisheksy.github.io',
+	  env,
+	}
+]);
